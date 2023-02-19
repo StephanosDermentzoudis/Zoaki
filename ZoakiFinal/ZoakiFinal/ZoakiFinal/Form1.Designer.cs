648335d6ccefd2f478329271c3453b30b4f58e1b
StephanosDermentzoudis <99821546+StephanosDermentzoudis@users.noreply.github.com>
2023-02-19 23:28:19 +0200

    Add files via upload

---
+++ ZoakiFinal/ZoakiFinal/ZoakiFinal/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ZoakiFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.capybara = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonext = new System.Windows.Forms.Button();
+            this.buttonnormal = new System.Windows.Forms.Button();
+            this.buttoneasy = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Stats = new System.Windows.Forms.Button();
+            this.ReturnBTN = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clicks: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // capybara
+            // 
+            this.capybara.BackColor = System.Drawing.Color.Transparent;
+            this.capybara.BackgroundImage = global::ZoakiFinal.Properties.Resources.capybara;
+            this.capybara.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.capybara.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.capybara.FlatAppearance.BorderSize = 0;
+            this.capybara.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.capybara.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.capybara.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.capybara.Location = new System.Drawing.Point(336, 166);
+            this.capybara.Name = "capybara";
+            this.capybara.Size = new System.Drawing.Size(102, 87);
+            this.capybara.TabIndex = 2;
+            this.capybara.UseVisualStyleBackColor = false;
+            this.capybara.Click += new System.EventHandler(this.capybara_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::ZoakiFinal.Properties.Resources.retry;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(716, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 64);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // buttonext
+            // 
+            this.buttonext.Location = new System.Drawing.Point(703, 12);
+            this.buttonext.Name = "buttonext";
+            this.buttonext.Size = new System.Drawing.Size(88, 25);
+            this.buttonext.TabIndex = 4;
+            this.buttonext.Text = "Extreme Mode";
+            this.buttonext.UseVisualStyleBackColor = true;
+            this.buttonext.Click += new System.EventHandler(this.buttonext_Click_1);
+            // 
+            // buttonnormal
+            // 
+            this.buttonnormal.Location = new System.Drawing.Point(609, 12);
+            this.buttonnormal.Name = "buttonnormal";
+            this.buttonnormal.Size = new System.Drawing.Size(88, 25);
+            this.buttonnormal.TabIndex = 5;
+            this.buttonnormal.Text = "Normal Mode";
+            this.buttonnormal.UseVisualStyleBackColor = true;
+            this.buttonnormal.Click += new System.EventHandler(this.buttonnormal_Click_1);
+            // 
+            // buttoneasy
+            // 
+            this.buttoneasy.Location = new System.Drawing.Point(515, 12);
+            this.buttoneasy.Name = "buttoneasy";
+            this.buttoneasy.Size = new System.Drawing.Size(88, 25);
+            this.buttoneasy.TabIndex = 6;
+            this.buttoneasy.Text = "Easy Mode";
+            this.buttoneasy.UseVisualStyleBackColor = true;
+            this.buttoneasy.Click += new System.EventHandler(this.buttoneasy_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.Yellow;
+            this.Save.Location = new System.Drawing.Point(12, 374);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(59, 28);
+            this.Save.TabIndex = 8;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click_1);
+            // 
+            // Stats
+            // 
+            this.Stats.BackColor = System.Drawing.Color.Yellow;
+            this.Stats.Location = new System.Drawing.Point(12, 410);
+            this.Stats.Name = "Stats";
+            this.Stats.Size = new System.Drawing.Size(59, 28);
+            this.Stats.TabIndex = 9;
+            this.Stats.Text = "Stats";
+            this.Stats.UseVisualStyleBackColor = false;
+            this.Stats.Click += new System.EventHandler(this.Stats_Click_1);
+            // 
+            // ReturnBTN
+            // 
+            this.ReturnBTN.BackColor = System.Drawing.Color.Yellow;
+            this.ReturnBTN.Location = new System.Drawing.Point(12, 195);
+            this.ReturnBTN.Name = "ReturnBTN";
+            this.ReturnBTN.Size = new System.Drawing.Size(59, 28);
+            this.ReturnBTN.TabIndex = 10;
+            this.ReturnBTN.Text = "Return";
+            this.ReturnBTN.UseVisualStyleBackColor = false;
+            this.ReturnBTN.Click += new System.EventHandler(this.ReturnBTN_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(213, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(439, 140);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ZoakiFinal.Properties.Resources.space;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ReturnBTN);
+            this.Controls.Add(this.Stats);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttoneasy);
+            this.Controls.Add(this.buttonnormal);
+            this.Controls.Add(this.buttonext);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.capybara);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Capybara";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button capybara;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonext;
+        private System.Windows.Forms.Button buttonnormal;
+        private System.Windows.Forms.Button buttoneasy;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Stats;
+        private System.Windows.Forms.Button ReturnBTN;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ZoakiFinal/ZoakiFinal/ZoakiFinal/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace ZoakiFinal
+{
+    public partial class Form1 : Form
+    {
+        String difficulty = "Medium";
+        int cclicks;
+        private int _ticks;
+        private int _clicks;
+        private int diskolia = 11;
+        int x, y;
+        public int[] Xarr = new int[720];
+        public int[] Yarr = new int[330];
+
+        public class animal
+        {
+            public string name = "name";
+        }
+
+        public class player
+        {
+            public string username;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            ReturnBTN.Location = new Point(10000, 200);
+            this.dataGridView1.Location = new Point(10000, 195);
+            Stats.Location = new Point(24, 416);
+            Save.Location = new Point(24, 387);
+
+            textBox1.Text = "Unnamed";
+            textBox1.Location = new Point(186, 100);
+            player Pplayer = new player();
+            Pplayer.username = textBox1.Text;
+
+            this.capybara.Location = new System.Drawing.Point(314, 136);
+            this.buttoneasy.Location = new System.Drawing.Point(379, 12);
+            this.buttonnormal.Location = new System.Drawing.Point(523, 12);
+            this.buttonext.Location = new System.Drawing.Point(663, 12);
+            for (int i = 20; i < Xarr.Length; i++)
+            {
+                Xarr.Append(i);
+            };
+            for (int i = 20; i < Yarr.Length; i++)
+            {
+                Yarr.Append(i);
+            };
+            animal Capybara = new animal();
+
+            Capybara.name = "Capybara";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //capybara
+            cclicks = 0;
+            Stats.Location = new Point(20004, 416);
+            Save.Location = new Point(20004, 387);
+            textBox1.Location = new Point(18006, 100);
+            this.buttoneasy.Location = new System.Drawing.Point(37900, 12);
+            this.buttonnormal.Location = new System.Drawing.Point(52300, 12);
+            this.buttonext.Location = new System.Drawing.Point(66300, 12);
+            timer1.Start();
+            timer2.Start();
+            label2.Text = "Clicks " + _clicks;
+            _clicks++;
+        }
+
+
+
+
+
+        private void buttonext_Click(object sender, EventArgs e)
+        {
+            difficulty = "Extreme";
+            diskolia = 6;
+            MessageBox.Show("Υπέρτατη δυσκολία έχεις 5 δευτερόλεπτα!");
+            this.timer2.Interval = 500;
+        }
+
+        private void buttonnormal_Click(object sender, EventArgs e)
+        {
+            difficulty = "Medium";
+            diskolia = 11;
+            MessageBox.Show("Μέτρια δυσκολία έχεις 10 δευτερόλεπτα!");
+            this.timer2.Interval = 1000;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+        private void ReturnBTN_Click(object sender, EventArgs e)
+        {
+            ReturnBTN.Location = new Point(10000, 200);
+            this.dataGridView1.Location = new Point(10000, 195);
+
+        }
+
+        private void capybara_Click(object sender, EventArgs e)
+        {
+            Stats.Location = new Point(20004, 416);
+            Save.Location = new Point(20004, 387);
+            textBox1.Location = new Point(18006, 100);
+            this.buttoneasy.Location = new System.Drawing.Point(37900, 12);
+            this.buttonnormal.Location = new System.Drawing.Point(52300, 12);
+            this.buttonext.Location = new System.Drawing.Point(66300, 12);
+            timer1.Start();
+            timer2.Start();
+            label2.Text = "Clicks " + _clicks;
+            _clicks++;
+        }
+
+        private void ReturnBTN_Click_1(object sender, EventArgs e)
+        {
+            ReturnBTN.Location = new Point(10000, 200);
+            this.dataGridView1.Location = new Point(10000, 195);
+        }
+
+
+
+        private void buttonnormal_Click_1(object sender, EventArgs e)
+        {
+            difficulty = "Medium";
+            diskolia = 11;
+            MessageBox.Show("Μέτρια δυσκολία έχεις 10 δευτερόλεπτα!");
+            this.timer2.Interval = 1000;
+        }
+
+        private void buttoneasy_Click(object sender, EventArgs e)
+        {
+            difficulty = "Easy";
+            diskolia = 16;
+            MessageBox.Show("Εύκολη δυσκολία έχεις 15 δευτερόλεπτα!");
+            this.timer2.Interval = 1500;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\stefan\\Documents\\ZoakiDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ZoakiDt values (@Username,@Clicks,@Difficulty)", con);
+            cmd.Parameters.AddWithValue("@Username", (textBox1.Text));
+            cmd.Parameters.AddWithValue("@Clicks", (cclicks.ToString()));
+            cmd.Parameters.AddWithValue("@Difficulty", difficulty);
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Saved Succesfully");
+        }
+
+        public void Stats_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\stefan\\Documents\\ZoakiDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from ZoakiDt", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            this.dataGridView1.Location = new System.Drawing.Point(193, 195);
+            ReturnBTN.Location = new System.Drawing.Point(25, 206);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //reset
+            timer1.Stop();
+            timer2.Stop();
+
+            Stats.Location = new Point(24, 416);
+            Save.Location = new Point(24, 387);
+            this.buttoneasy.Location = new System.Drawing.Point(379, 12);
+            this.buttonnormal.Location = new System.Drawing.Point(523, 12);
+            this.buttonext.Location = new System.Drawing.Point(663, 12);
+            _clicks = 0;
+            _ticks = 0;
+            label1.Text = "Timer: 0";
+            label2.Text = "Clicks: 0";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            //timer
+            _ticks++;
+            label1.Text = "Time: " + _ticks;
+
+            if (_ticks == diskolia)
+            {
+                timer1.Stop();
+                timer2.Stop();
+
+                cclicks = _clicks;
+                Stats.Location = new Point(24, 416);
+                Save.Location = new Point(24, 387);
+                this.buttoneasy.Location = new System.Drawing.Point(379, 12);
+                this.buttonnormal.Location = new System.Drawing.Point(523, 12);
+                this.buttonext.Location = new System.Drawing.Point(663, 12);
+                label1.Text = "Game Over";
+                label2.Text = "Your clicks: " + _clicks;
+                _ticks = 0;
+                _clicks = 0;
+                MessageBox.Show("Time is up!");
+            }
+        }
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            x = rand.Next(Xarr.Length);
+            y = rand.Next(Yarr.Length);
+            this.capybara.Location = new System.Drawing.Point(x, y);
+        }
+
+        private void buttonext_Click_1(object sender, EventArgs e)
+        {
+            difficulty = "Extreme";
+            diskolia = 6;
+            MessageBox.Show("Υπέρτατη δυσκολία έχεις 5 δευτερόλεπτα!");
+            this.timer2.Interval = 500;
+        }
+
+
+
+
+    }
+}
+    
+
+
+
